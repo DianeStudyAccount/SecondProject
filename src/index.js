879b86b5f241dd102b3e78e12ca47e84a1deb044
Diana Shimlykh <d56671250@gmail.com>
2026-02-19 00:23:27 +0300

    Add:webpack set

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import oneLog from "./modules/one.js";
+import twoLog from "./modules/two.js";
+
+oneLog();
+twoLog();
---
+++ webpack.config.cjs
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.js",
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "production",
+
+  devServer: {
+    hot: true,
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+      watch: true,
+    },
+  },
+};

298d61f12136701aad276bdbe1f2ebfc11703965
Diana Shimlykh <d56671250@gmail.com>
2026-02-22 00:10:38 +0300

    add: smooth behaviour

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ import "./src/css/style.min.css";
 import timer from "./src/modules/timer";
 import menu from "./src/modules/menu";
 import modalWindow from "./src/modules/modalWindow"
+import smooth from "./src/modules/smoothScroll";
 
 timer("23 february 2026");
 menu();
 modalWindow();
+smooth();

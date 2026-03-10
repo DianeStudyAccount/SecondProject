65bab75e6954d5f5734ae8a68555a16f70524ff2
Diana Shimlykh <d56671250@gmail.com>
2026-03-10 21:42:39 +0300

    add: XMLHttpRequest for practice

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-const sendData = (url, data) => {
-  return fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-    body: JSON.stringify(data),
-  }).then((response) => response.json());
+let req = new XMLHttpRequest();
+
+req.open("POST", "https://jsonplaceholder.typicode.com/posts");
+
+req.setRequestHeader("Content-Type", "application/json");
+
+req.responseType = "json";
+
+req.onload = () => {
+  if (req.status >= 200 && req.status < 300) {
+    console.log(req.response);
+  } else {
+    console.log(`Error ${req.status}: ${req.statusText}`);
+  }
 };
 
-const getData = (url) => {
-  return fetch(url).then((response) => response.json());
+req.onprogress = (e) => {
+  if (e.lengthComputable) {
+    console.log(`Received ${e.loaded} of ${e.total} bytes`);
+  } else {
+    console.log(`Received ${e.loaded} bytes`);
+  }
+};
+req.onerror = () => {
+  alert("Request failed");
+};
+const data = {
+  title: "user",
+  body: {
+    username: "John123",
+    password: 456123,
+  },
+  userId: 1,
 };
 
-getData("./db.json")
-  .then((data) => {
-    return sendData("https://jsonplaceholder.typicode.com/posts", data);
-  })
-  .then((result) => {
-    console.log("Sent:", result);
-  })
-  .catch((error) => {
-    console.error("Ошибка:", error);
-  });
+req.send(JSON.stringify(data));
